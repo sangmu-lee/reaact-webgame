ebae9d2f00179086dab009457e02ecab1e8d0987
leessang10 <leessang10@naver.com>
2022-06-26 17:14:08 +0900

    Initial commit

---
+++ lecture/WordRelay.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class WordRelay extends Component {
+    state = {
+        text: 'Hello, webpack',
+    }
+    render() {
+        return (
+            <div>
+                <h1>{this.state.text}</h1>
+            </div>
+        );
+    }
+}
+
+module.exports = WordRelay;
